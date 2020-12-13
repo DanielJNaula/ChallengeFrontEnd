4573c68554c4d055b0f09825a6a473e964664c1e
= <=>
2020-12-12 23:22:52 -0500

    Diseño de la pagina buscador e implementacion de vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tickets:[
+      {nombre_aerolinea: 'aerolínea 1', precio:125.62, numero_escalas:2 , duracion_vuelo:'6 horas y 30 min'},
+      {nombre_aerolinea: 'aerolínea 2', precio:255.62, numero_escalas:1 , duracion_vuelo:'4 horas y 30 min'},
+      {nombre_aerolinea: 'aerolínea 3', precio:455.62, numero_escalas:0 , duracion_vuelo:'2 hora'},
+      {nombre_aerolinea: 'aerolínea 4', precio:155.62, numero_escalas:2 , duracion_vuelo:'4 hora y 50 min'},
+      {nombre_aerolinea: 'aerolínea 6', precio:245.62, numero_escalas:2 , duracion_vuelo:'5 hora y 35 min'},
+      {nombre_aerolinea: 'aerolínea 7', precio:195.62, numero_escalas:0 , duracion_vuelo:'2 hora y 50 min'},
+      {nombre_aerolinea: 'aerolínea 10', precio:145.62, numero_escalas:1 , duracion_vuelo:'3 hora y 10 min'},
+      {nombre_aerolinea: 'aerolínea 15', precio:225.62, numero_escalas:6 , duracion_vuelo:'9 hora y 25 min'},
+    ]
   },
   mutations: {
   },
--- src/views/Buscador.vue
+++ src/views/Buscador.vue
@@ -1,11 +1,128 @@
 <template>
   <v-container>
-      <h1>Hola mundito</h1>
+    <v-row>
+        <v-col cols="12" sm="12" md="3">
+          <h2>Buscador</h2> <br>
+          <v-text-field
+            outlined
+            label="Ciudad origen"
+            append-outer-icon="edit_location"
+          >
+          </v-text-field>
+          <v-text-field
+            outlined
+            label="Ciudad destino"
+            append-outer-icon="pin_drop"
+          ></v-text-field>
+          <v-dialog
+            ref="dialog"
+            v-model="modal"
+            :return-value.sync="date"
+            persistent
+            width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                outlined
+                v-model="date"
+                label="Fecha de salida"
+                append-outer-icon="date_range"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="modal = false"
+              >
+                cerrar
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.dialog.save(date)"
+              >
+                listo
+              </v-btn>
+            </v-date-picker>
+          </v-dialog>
+          <v-dialog
+            ref="dialog"
+            v-model="modal"
+            :return-value.sync="date"
+            persistent
+            width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+              outlined
+                v-model="date"
+                label="Fecha de retorno"
+                append-outer-icon="date_range"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="modal = false"
+              >
+                Cerrar
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.dialog.save(date)"
+              >
+                Listo
+              </v-btn>
+            </v-date-picker>
+          </v-dialog>
+          <div class="text-center" >
+            <router-link to="/about" class="v-btn v-btn--rounded theme--dark elevation-6 v-size--x-large primary">Buscar
+              <v-icon
+                right
+                dark
+              >
+                search
+              </v-icon></router-link>
+            
+          </div>
+        </v-col>
+        <v-col cols="12" sm="12" md="9">
+            <Cards></Cards>
+        </v-col>
+    </v-row>
+    
   </v-container>
+  
 </template>
 
 <script>
+import Cards from '@/components/buscador/Cards.vue';
 export default {
+  components: {
+    Cards
+  },
+  data: () => ({
+      date: new Date().toISOString().substr(0, 10),
+      modal: false,
+     
+  }),
 
 }
 </script>
